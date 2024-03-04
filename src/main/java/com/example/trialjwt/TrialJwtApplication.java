a52ef4bee47845a11d246c35e236e8d63f88f5dd
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-03-04 14:06:15 +0300

    initial commit

---
+++ src/main/java/com/example/trialjwt/TrialJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.trialjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrialJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trialjwt/TrialJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trialjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
