a3a5813405fad187bcd4d5c21695d2c82e5fedc3
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-03-05 09:41:53 +0300

    starts work on jwt filter

---
+++ src/main/java/com/example/trialjwt/config/JwtFilter.java
@@ -0,0 +1,24 @@
+package com.example.trialjwt.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter{
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (header != null || !header.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+            } else {
+                String token = header.substring(7);
+
+            }
+    }
+}
--- src/main/java/com/example/trialjwt/config/SecurityConfig.java
+++ src/main/java/com/example/trialjwt/config/SecurityConfig.java
@@ -9,12 +9,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
